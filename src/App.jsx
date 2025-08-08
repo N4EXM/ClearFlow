d523c9ad7bcbf43c7eeafbcc0c91c8a7fac7d514
N4EXM <naemmmail89@gmail.com>
2025-08-08 22:01:07 +0100

    added a loading state to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import { useState } from 'react'
+/**
+ * 
+ * This is the main component which contains all the data and routes to other pages
+ * 
+ * @
+ * 
+ */
+
+
+import { useEffect, useState } from 'react'
 import './App.css'
 import LoadingPage from './pages/LoadingPage'
+import Home from './pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(true)
+    }, 3000)
+  }, [])
+
   return (
-    <LoadingPage/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={"/"}
+          index
+          element={
+            loading 
+            ? <Home
+                loading={loading}
+              /> 
+            : <LoadingPage/>}
+        />
+        <Route/>
+        <Route/>
+        <Route/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = ({loading}) => {
+  return (
+    <div
+        className='bg-BG min-h-screen h-full w-full text-CText duration-200 '
+    >
+        Home
+    </div>
+  )
+}
+
+export default Home
