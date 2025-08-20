51c611aa84b04c0e6535c66af79fd47a9aa00a56
N4EXM <naemmmail89@gmail.com>
2025-08-20 16:33:01 +0100

    still working on the task card error handling

--- src/components/Project/NewTaskCard.jsx
+++ src/components/Project/NewTaskCard.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from 'react'
 
 const NewTaskCard = ({setIsNewTaskActive, handleAddingTasks, minDate, maxDate}) => {
 
+    // toggles
+    const [isError, setIsError] = useState(false)
+
+    // task details
     const [title, setTitle] = useState("")
     const [desc, setDesc] = useState("")
     const [date, setDate] = useState("")
@@ -20,19 +24,27 @@ const NewTaskCard = ({setIsNewTaskActive, handleAddingTasks, minDate, maxDate}) 
         setTitle("")
         setDesc("")
         setDate("")
+        setIsError(false)
     }
 
     const addingTask = () => {
 
-        if (title === "" || desc === "" || date === "")
-
-        handleAddingTasks(title, desc, date)
-        handleNewTaskClose()
+        if (title === "" || desc === "" || date == "" || (date > maxDate || date < minDate)) {
+            setIsError(true)
+            console.log("ran")
+        }
+        else {
+            setIsError(false)
+            handleAddingTasks(title, desc, date)
+            handleNewTaskClose() 
+        }
+       
     }
 
     useEffect(() => {
         if (maxDate !== currentProjectDate) {
             setDate("")
+            setIsError(true)
         }
     }, [maxDate])
 
@@ -42,8 +54,13 @@ const NewTaskCard = ({setIsNewTaskActive, handleAddingTasks, minDate, maxDate}) 
     >
         
         <div
-            className='w-fit h-full flex flex-col gap-1'
+            className='w-fit h-full flex flex-col gap-1 relative'
         >
+            <i
+                className={`p-2 absolute bg-BGS -top-1.5 -right-1.5 ${isError ? "block" : "hidden"} z-10`}
+            >
+                <svg className='text-rose-500' xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><g fill="none"><path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"/><path fill="currentColor" d="m13.299 3.148l8.634 14.954a1.5 1.5 0 0 1-1.299 2.25H3.366a1.5 1.5 0 0 1-1.299-2.25l8.634-14.954c.577-1 2.02-1 2.598 0M12 4.898L4.232 18.352h15.536zM12 15a1 1 0 1 1 0 2a1 1 0 0 1 0-2m0-7a1 1 0 0 1 1 1v4a1 1 0 1 1-2 0V9a1 1 0 0 1 1-1"/></g></svg>
+            </i>
             <input 
                 onChange={(e) => handleTitleChange(e)}
                 value={title}
@@ -62,7 +79,7 @@ const NewTaskCard = ({setIsNewTaskActive, handleAddingTasks, minDate, maxDate}) 
         </div>
 
         <div
-            className='flex item-end justify-end w-full h-full'
+            className='flex item-end justify-end w-full h-full '
         >
             <div
                 className='flex flex-row items-end justify-between gap-1 w-full'
